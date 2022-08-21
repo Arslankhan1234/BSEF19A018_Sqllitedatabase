5944942cb39d5bfb7a4af5b79f09573b628ce95d
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-08-21 14:53:55 -0700

    creating Student class with  attributes and getter setter

--- app/src/test/java/Models/Student.java
+++ app/src/test/java/Models/Student.java
@@ -2,6 +2,33 @@ package Models;
 
 public class Student {
 
-    public  String Name;
-    public  int Age;
+    private String name;
+    private int rollNmber;
+
+
+    public Student(String name, int rollNmber) {
+        this.name = name;
+        this.rollNmber = rollNmber;
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNmber() {
+        return rollNmber;
+    }
+
+    public void setRollNmber(int rollNmber) {
+        this.rollNmber = rollNmber;
+    }
+
+
+
 }
