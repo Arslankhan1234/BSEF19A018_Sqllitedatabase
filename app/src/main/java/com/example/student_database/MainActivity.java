3b4609103fc5a44a811d26ecf952e542268964d5
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-08-21 18:52:35 -0700

    In main.java declared variable and assigned by findviewbyid

--- app/src/main/java/com/example/student_database/MainActivity.java
+++ app/src/main/java/com/example/student_database/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText name,age;
+    EditText name,roll;
     Button add,view,update,delete;
+    Switch switchisactive;
+    ListView listViewStudent;
 
 
     @Override
@@ -19,9 +23,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         name=findViewById(R.id.name);
-        age=findViewById(R.id.age);
+        roll=findViewById(R.id.rollno);
         add=findViewById(R.id.add);
         view=findViewById(R.id.view);
+        switchisactive=findViewById(R.id.active);
+        listViewStudent=findViewById(R.id.list);
+
         DbHelper db=new DbHelper(MainActivity.this);
 
 
